954c247ab3b33970dd3e95b2740cd5ed8cc808e1
prashantisrishu <53404310+prashantisrishu@users.noreply.github.com>
2020-06-26 23:11:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import SignIn from './SignINpage'
+import Profile from './profilepage'
+import {BrowserRouter as Router ,Route} from 'react-router-dom'
 
 class App extends React.Component{
   render(){
     return(
-        <SignIn/>
+      <Router>
+        <Route path="/" exact component={SignIn}/>
+        <Route path="/profile" exact component={Profile}/>
+      </Router>
     )
   }
 }
